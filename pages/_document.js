c1c6d67095b1bd1f798c74c971a8cb9dbeba64ea
matheusgustavo16 <matheus.gustavo@hotmail.com.br>
2023-05-25 23:25:48 -0300

    lang-ptbr

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import {Head, Html, Main, NextScript} from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="pt-BR">
       <Head>
         <meta property="og:title" content="Copy Online" key="title"/>
         <meta property="og:description" content="Utilizando o poder da Inteligência Artificial, você pode gerar copys e conteúdos de alta qualidade de forma rápida e eficiente, teste grátis já." key="description"/>
